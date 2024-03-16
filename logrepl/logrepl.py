84f4db84be92943aa531118120b6386ae6f3189c
David Vincelli <david.vincelli@gmail.com>
2024-03-16 19:56:01 -0400

    Upgrage to psycopg 3

--- logrepl/logrepl.py
+++ logrepl/logrepl.py
@@ -1,6 +1,6 @@
 import subprocess
-import psycopg2
-from psycopg2 import sql
+import psycopg
+from psycopg import sql
 import argparse
 import configparser
 import io
@@ -13,53 +13,84 @@ def run_subprocess(command, env=None):
 
 @contextlib.contextmanager
 def connect_db(db, user, password, host, port):
-    with psycopg2.connect(dbname=db, user=user, host=host, port=port, password=password) as cxn:
+    with psycopg.connect(dbname=db, user=user, host=host, port=port, password=password, sslmode="require") as cxn:
         yield cxn
 
 
 @contextlib.contextmanager
-def source_db(config):
+def source_db(config, dbname=None):
     conf = config["source"]
     with connect_db(
-        conf["dbname"], conf["username"], conf["password"], conf["host"], conf["port"]
+        dbname or conf["dbname"], conf["username"], conf["password"], conf["host"], conf["port"]
     ) as conn:
         yield conn
 
 
 @contextlib.contextmanager
-def target_db(config):
+def target_db(config, dbname=None):
     conf = config["target"]
     with connect_db(
-        conf["dbname"], conf["username"], conf["password"], conf["host"], conf["port"]
+        dbname or conf["dbname"], conf["username"], conf["password"], conf["host"], conf["port"]
     ) as conn:
         yield conn
 
 
-def execute_sql(conn, query):
+def execute_sql(conn, query, args=None):
+    print(query, args)
+    args = args or []
     with conn.cursor() as cur:
-        cur.execute(query)
+        cur.execute(query, args)
     conn.commit()
 
 
-def dump_schema(db, user, password, host, port, schema, file):
-    command = f"pg_dump -h {host} -p {port} -U {user} -s {db} -n {schema} > {file}"
-    run_subprocess(command, env={"PGPASSWORD": password})
+def dump_schema(config, file="/tmp/schema.sql"):
+    dbname = config["source"]["dbname"]
+    user = config["source"]["username"]
+    password = config["source"]["password"]
+    host = config["source"]["host"]
+    port = config["source"]["port"]
+    sslmode = config["source"].get("sslmode", "require")
+    schema = 'public'
+
+    command = f"pg_dump -h {host} -p {port} -U {user} -s {dbname} -n {schema} -x -O > {file}"
+    run_subprocess(command, env={"PGPASSWORD": password, "PGSSLMODE": sslmode})
 
 
-def restore_schema(db, user, password, host, port, file):
-    command = f"psql -h {host} -p {port} -U {user} -d {db} -f {file}"
-    run_subprocess(command, env={"PGPASSWORD": password})
+def restore_schema(config, file="/tmp/schema.sql"):
+    create_database(config)
+
+    dbname = config["target"]["dbname"]
+    user = config["target"]["username"]
+    password = config["target"]["password"]
+    host = config["target"]["host"]
+    port = config["target"]["port"]
+    sslmode = config["source"].get("sslmode", "require")
+
+    command = f"psql -h {host} -p {port} -U {user} -d {dbname} -f {file}"
+    run_subprocess(command, env={"PGPASSWORD": password, "PGSSLMODE": sslmode})
 
 
 def create_pglogical_extension(conn):
     execute_sql(conn, "CREATE EXTENSION IF NOT EXISTS pglogical")
 
 
-def create_node(conn, node, host, port, db, user, password):
+def source_dsn(config):
+    source = config["source"]
+    sslmode = config["source"].get("sslmode", "require")
+    return f"host={source['host']} port={source['port']} dbname={source['dbname']} user={source['username']} password={source['password']} sslmode={sslmode}"
+
+
+def target_dsn(config):
+    target = config["target"]
+    sslmode = config["target"].get("sslmode", "require")
+    return f"host={target['host']} port={target['port']} dbname={target['dbname']} user={target['username']} password={target['password']} sslmode={sslmode}"
+
+
+def create_node(conn, node, dsn):
     execute_sql(
         conn,
         sql.SQL("SELECT pglogical.create_node(node_name := %s, dsn := %s)"),
-        [node, f"host={host} port={port} dbname={db} user={user} password={password}"],
+        [node, dsn],
     )
 
 
@@ -79,23 +110,13 @@ def add_all_tables_to_replication_set(conn, set_name):
     )
 
 
-def create_subscription(conn, subscription, host, port, db, user, password, set_name):
+def create_subscription(conn, subscription, dsn, set_name):
     execute_sql(
         conn,
         sql.SQL(
             "SELECT pglogical.create_subscription(subscription_name := %s, provider_dsn := %s, replication_sets := ARRAY[%s])"
         ),
-        [subscription, f"host={host} port={port} dbname={db} user={user} password={password}", set_name],
-    )
-
-
-def start_subscription(conn, subscription):
-    execute_sql(
-        conn,
-        sql.SQL(
-            "SELECT pglogical.alter_subscription_set_enabled(subscription_name := %s, enabled := true)"
-        ),
-        [subscription],
+        [subscription, dsn, set_name],
     )
 
 
@@ -124,8 +145,8 @@ def wait_for_subscription(conn, subscription):
         for record in cur.fetchall():
             print(record)
 
-# -- commands --
 
+# -- commands --
 
 
 def status(config):
@@ -134,54 +155,65 @@ def status(config):
 
 
 def setup(config):
-    source_db = config["source"]["dbname"]
-    source_user = config["source"]["username"]
-    source_password = config["source"]["password"]
-    source_host = config["source"]["host"]
-    source_port = config["source"]["port"]
-    source_node = config["source"]["node"]
+    create_extension(config) # on both source and target
+    create_schema(config) # from source to target
+    create_provider_node(config)  # on source database
 
-    target_db = config["target"]["dbname"]
-    target_user = config["target"]["username"]
-    target_password = config["target"]["password"]
-    target_host = config["target"]["host"]
-    target_port = config["target"]["port"]
-    target_node = config["target"]["node"]
-    target_subscription = config["target"]["subscription"]
-
-    # Ensure the pglogical extension is installed on both databases
-    with source_db(source_db, source_user, source_host, source_port) as conn:
-        create_pglogical_extension(conn)
-
-    with target_db(target_db, target_user, target_host, target_port) as conn:
-        create_pglogical_extension(conn)
-
-    # Dump the schema from the source database
-    schema_file = "/tmp/schema.sql"
-    dump_schema(source_db, source_user, source_password, source_host, source_port, "public", schema_file)
-    # Restore the schema to the target database
-    restore_schema(target_db, target_user, target_password, target_host, target_port, schema_file)
-
-    # create the provider nodes on the source databases
-    with source_db(source_db, source_user, source_host, source_port) as conn:
-        create_node(conn, source_node, source_host, source_port, source_db, source_user, source_password)
-
-    set_name = "default"
     # Add all tables in public schema to the default replication set on the source databases
-    with source_db(source_db, source_user, source_host, source_port) as conn:
+    init_replication_set(config)
+    create_subscriber(config)
+
+    status(config)
+
+
+def create_subscriber(config):
+    set_name = config["source"]["replication_set"]
+    with target_db(config) as conn:
+        try:
+            create_node(conn, config["target"]["node"], target_dsn(config))
+        except psycopg.errors.InternalError_ as e: # node mlflow_staging_subscriber already exists
+            print(e)
+
+    with target_db(config) as conn:
+        create_subscription(
+            conn, config["target"]["subscription"], source_dsn(config), set_name
+        )
+
+
+def init_replication_set(config):
+    set_name = config["source"]["replication_set"]
+    with source_db(config) as conn:
         create_replication_set(conn, set_name)
         add_all_tables_to_replication_set(conn, set_name)
 
-    # Create the subscriber on the target database.
-    with target_db(target_db, target_user, target_host, target_port) as conn:
-        create_node(conn, target_node, target_host, target_port, target_db, target_user, target_password)
-        create_subscription(
-            conn, target_subscription, source_host, source_port, source_db, source_user, set_name
-        )
 
-    # Check the status of the subscription
-    with target_db(target_db, target_user, target_host, target_port) as conn:
-        subscription_status(conn, target_subscription)
+def create_provider_node(config):
+    # create the provider nodes on the source databases
+    with source_db(config) as conn:
+        create_node(conn, config["source"]["node"], source_dsn(config))
+
+
+def create_schema(config):
+    dump_schema(config)
+    restore_schema(config)
+
+
+def create_extension(config):
+    with source_db(config) as conn:
+        create_pglogical_extension(conn)
+
+    with target_db(config) as conn:
+        create_pglogical_extension(conn)
+
+
+def create_database(config):
+    dbname = config["target"]["dbname"]
+    with target_db(config, dbname="template1") as conn:
+        conn.autocommit = True
+        with conn.cursor() as cursor:
+            cursor.execute(sql.SQL("CREATE DATABASE {}").format(
+                sql.Identifier(dbname)), 
+            )
 
 
 def start(config):
@@ -211,21 +243,21 @@ def teardown(config):
 def verify_config(config):
     print("Verifying the configuration")
 
-    print("Verifying the source database")
     with source_db(config) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT 1")
             result = cur.fetchone()
             if result != (1,):
                 raise SystemExit("Source database is not accessible")
+    print("Source database is accessible")
 
-    print("Verifying the target database")
-    with target_db(config) as conn:
+    with target_db(config, dbname="template1") as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT 1")
             result = cur.fetchone()
             if result != (1,):
                 raise SystemExit("Target database is not accessible")
+    print("Target database is accessible")
 
 
 def wait(config):
@@ -250,7 +282,28 @@ def argparser():
     subparsers = parser.add_subparsers(help="sub-command help", dest="command")
 
     subparsers.add_parser("config", help="Print the configuation")
-    subparsers.add_parser("setup", help="Setup the replication")
+    subparsers.add_parser("dump", help="Dump the schema")
+    subparsers.add_parser("load", help="Restore the schema")
+
+    setup_subparser = subparsers.add_parser("setup", help="Setup the replication")
+    setup_subparsers = setup_subparser.add_subparsers(dest="setup_command")
+    setup_subparsers.add_parser("all", help="Setup the replication")
+    setup_subparsers.add_parser("extension", help="Create the pglogical extension")
+
+    schema_parser = setup_subparsers.add_parser("schema", help="Create the schema")
+    schema_parser.add_argument(
+        "--file", "-f", required=False, help="Path to the schema file", default="/tmp/schema.sql"
+    )
+    schema_parser.add_argument(
+        "--dump", "-d", required=False, help="Dump the schema from source database"
+    )
+    schema_parser.add_argument(
+        "--load", "-l", required=False, help="Load the schema on target database"
+    )
+    setup_subparsers.add_parser("node", help="Create the provider node")
+    setup_subparsers.add_parser("replication_set", help="Create the replication set")
+    setup_subparsers.add_parser("subscriber", help="Create the subscriber")
+
     subparsers.add_parser("start", help="Start the replication")
     subparsers.add_parser(
         "status", help="Show the status of the replication"
@@ -263,6 +316,26 @@ def argparser():
     return parser
 
 
+def setup_command(config, args):
+    if args.setup_command == "all":
+        setup(config)
+    elif args.setup_command == "extension":
+        create_extension(config)
+    elif args.setup_command == "schema":
+        if args.dump:
+            dump_schema(config, args.file)
+        elif args.load:
+            restore_schema(config, args.file)
+        else:
+            parser.print_help()
+    elif args.setup_command == "node":
+        create_provider_node(config)
+    elif args.setup_command == "replication_set":
+        init_replication_set(config)
+    elif args.setup_command == "subscriber":
+        create_subscriber(config)
+
+
 def main():
     parser = argparser()
     args = parser.parse_args()
@@ -272,10 +345,14 @@ def main():
 
     if args.command is None:
         parser.print_help()
+    elif args.command == "dump":
+        dump_schema(config)
+    elif args.command == "load":
+        restore_schema(config)
     elif args.command == "status":
         status(config)
     elif args.command == "setup":
-        setup(config)
+        setup_command(config, args)
     elif args.command == "start":
         start(config)
     elif args.command == "stop":
